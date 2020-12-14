789b33fed6e27b68cefa7ddfa9d47409c6277cc0
Ankit Modi - Mac <ankitm2@fractalanalytics.com>
2020-12-14 20:55:15 +0530

    sorted by rating, filtered by year

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import requests
 
 
 MAIN_URL = 'https://www.goodreads.com/review/list/13487053-ankit-modi?order=d&ref=nav_mybooks&shelf=read&sort=date_read&utf8=%E2%9C%93'
-YEAR = '2019'
-
+YEAR = '2020'
+OUTPUT_MD_FILE_PATH = '/Users/ankitmodi/Desktop/Code/ankitmodi.github.io/_posts/2020-12-14-my-year-in-books-2019.md'
+INTRO_PARA_OF_BLOG = f'{YEAR} was a good year of reading for me. Here are the books that I read in order of my rating:\n'
 
 def get_html_using_selenium(url):
-    
+
     # driver = webdriver.Chrome(executable_path='/Users/ankitmodi/Desktop/Code/goodreads_year_end_review/chromedriver')
     # driver.get (url)
     #
@@ -76,33 +77,51 @@ def get_books_data(soup):
         book_dict['rating'] = rating
 
         #review
-        td = tr.find_all('td', {'class':'field review'})[0]
-        span = td.find_all('span', {'style':'display:none'})[0]
-        lines = [str(i) for i in span.contents]
-        review = ' '.join(lines)
+        review = ''
+        td = tr.find_all('td', {'class':'field review'})
+        if(len(td) > 0):
+            td = td[0]
+            span = td.find_all('span')
+            if(len(span) > 0):
+                span = span[-1]
+                lines = [str(i) for i in span.contents]
+                review = ' '.join(lines)
         book_dict['review'] = review
 
-
+        #date_read
+        td = tr.find_all('td', {'class':'field date_read'})[0]
+        span = td.find_all('span', {'class':'date_read_value'})[0]
+        date_read = span.text
+        book_dict['date_read'] = date_read
 
         book_list.append(book_dict)
-        break
+        # break
+
     return book_list
+
+
+def filter_and_sort_books(book_list, year):
+    filtered_list = [i for i in book_list if year in i['date_read']]
+    sorted_list = sorted(filtered_list, key=lambda k: k['rating'], reverse=True)
+    return sorted_list
+
+
+def create_markdown(book_list, year, intro_para, md_file_path):
+    with open(md_file_path, 'w') as f:
+        f.write('---\n')
+        f.write(f'title: My Year in Books: {year}\n')
+        # f.write(f'excerpt: {year} was a good year in terms of reading\n')
+        f.write('---\n')
+        f.write('intro_para\n')
 
 
 
 if __name__ == '__main__':
     html_str = get_html_using_selenium(MAIN_URL)
 
-    # with open('tmp.html', 'w') as f:
-    #     f.write(html_str)
-
     soup = BeautifulSoup(html_str, 'lxml')
     book_list = get_books_data(soup)
-    print(book_list)
-
-
-
-    # response = requests.get(MAIN_URL)
-    # html_text = response.text
-    # with open('tmp.html', 'w') as f:
-    #     f.write(html_text)
+    # print(book_list)
+    filtered_sorted_book_list = filter_and_sort_books(book_list, YEAR)
+    # filter by year, sort by rating
+    # create_markdown(book_list, YEAR, INTRO_PARA_OF_BLOG, OUTPUT_MD_FILE_PATH)
